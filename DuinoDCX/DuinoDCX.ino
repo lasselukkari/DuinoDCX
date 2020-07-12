e33145ed5f9571f75b4f2c3a40c854473aabd223
Lasse Lukkari <lasse.lukkari@gmail.com>
2020-07-12 11:07:10 +0300

    Bumb version

--- DuinoDCX/DuinoDCX.ino
+++ DuinoDCX/DuinoDCX.ino
@@ -7,7 +7,7 @@
 #include "StaticFiles.h"
 #include "Ultradrive.h"
 
-#define VERSION "v0.0.35"
+#define VERSION "v0.0.36"
 #define BUILD_DATE __DATE__ " " __TIME__
 
 #define DEFAULT_AUTH "Basic RENYMjQ5NjpVbHRyYWRyaXZl" // DCX2496:Ultradrive in base64
